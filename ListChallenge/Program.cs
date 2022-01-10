01303202dddc9e1e0b2670b2e6ec716050fddb9c
16ashtot <16ashtot@fulford.york.sch.uk>
2022-01-10 13:34:02 +0000

    closes #1

--- ListChallenge/Program.cs
+++ ListChallenge/Program.cs
@@ -10,7 +10,7 @@ namespace ListChallenge
     {
         static void Main(string[] args)
         {
-            // add all names
+            // add all names again
             List<string> names = new List<string> { "Ethan", "Will", "Ashton", "Ben", "Tom", "Henry", "Cormac", "Will", "Adam"};
 
             Console.WriteLine(names[3]);
